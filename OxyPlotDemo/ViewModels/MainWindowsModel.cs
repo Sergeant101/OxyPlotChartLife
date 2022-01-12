fa5a02d70555ce43d7815c7a082993d57ad5e9ba
Sergey <s.v.zagorodnev@gmail.com>
2022-01-12 21:09:02 +0500

    first

---
+++ OxyPlotDemo/ViewModels/MainWindowsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using OxyPlot.Annotations;
+
+namespace OxyPlotDemo.ViewModels
+{
+    class MainWindowsModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
